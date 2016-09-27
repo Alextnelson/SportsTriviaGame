f758e5b05779eb3145e2889e6512eea2c369173e
Alexander Nelson <alexandernelson@alexandersmbp2>
2016-09-27 13:28:32 -0500

    Update model and helper methods for view controller

--- SportsTriviaGame/TriviaQuestions.swift
+++ SportsTriviaGame/TriviaQuestions.swift
@@ -9,4 +9,57 @@
 import Foundation
 
 
+enum SportsTriviaError: Error {
+    case invalidResource
+    case conversionError
+    case invalidKey
+}
+
+class SportsTrivia {
+    let description: String
+    let year: Int
+    let url: String
+
+    init(description: String, year: Int, url: String) {
+        self.description = description
+        self.year = year
+        self.url = url
+    }
+}
+
+class PlistConverter {
+    class func arrayFromFile(resource: String, ofType type: String) throws -> [[String : String]] {
+
+        guard let path =
+            Bundle.main.path(forResource: resource, ofType: type) else {
+                throw SportsTriviaError.invalidResource
+        }
+
+        guard let array = NSArray(contentsOfFile: path),
+            let castArray = array as? [[String: String]] else {
+                throw SportsTriviaError.conversionError
+        }
+        return castArray
+    }
+}
+
+
+class SportsTriviaUnarchiver {
+    class func sportsTriviaFromArray(array: [[String : String]]) -> [SportsTrivia] {
+
+        var sportsTriviaArray: [SportsTrivia] = []
+
+        for sportsTriviaEvent in array {
+            if let description = sportsTriviaEvent["description"], let year = sportsTriviaEvent["year"],
+                let yearInt = Int(year),
+                let url = sportsTriviaEvent["url"] {
+                let newSportsTriviaEvent = SportsTrivia(description: description, year: yearInt, url: url)
+                sportsTriviaArray.append(newSportsTriviaEvent)
+            }
+        }
+        return sportsTriviaArray
+    }
+}
+
+
 
--- SportsTriviaGame/TriviaViewController.swift
+++ SportsTriviaGame/TriviaViewController.swift
@@ -7,14 +7,95 @@
 //
 
 import UIKit
+import GameKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+
+    // MARK: Outlets
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var instructionLabel: UILabel!
+    @IBOutlet weak var shakeToCompleteInstructions: UILabel!
+
+    @IBOutlet weak var eventButtonOne: UIButton!
+    @IBOutlet weak var eventButtonTwo: UIButton!
+    @IBOutlet weak var eventButtonThree: UIButton!
+    @IBOutlet weak var eventButtonFour: UIButton!
+
+    var sportsTriviaQuiz: [SportsTrivia]
+    var eventButtons: [UIButton] = []
+
+    let roundsPerGame = 6
+    let eventsPerRound = 4
+    var roundPlayed = 0
+    var score = 0
+    var correctAnswers = 0
+    
+    var seconds = 60
+    var timer = Timer()
+    var timerRunning = false
+
+    var correctAnswerSound: SystemSoundID = 0
+    var wrongAnswerSound: SystemSoundID = 0
+
+    // Initializer to unarchive plist file of sports trivia
+    required init?(coder aDecoder: NSCoder) {
+        do {
+            let array = try PlistConverter.arrayFromFile(resource: "SportsEvents", ofType: "plist")
+            let sportsTriviaEvents = SportsTriviaUnarchiver.sportsTriviaFromArray(array: array)
+            self.sportsTriviaQuiz = sportsTriviaEvents
+        } catch let error {
+            fatalError("\(error)")
+        }
+        super.init(coder: aDecoder)
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
     }
-    
+
+    func startNewRound() {
+        let newRoundOfTrivia = createSportsTriviaOptions(trivia: self.sportsTriviaQuiz)
+        _ = sortedTrivia(trivia: newRoundOfTrivia)
+        _ = setUpEventButtons(trivia: newRoundOfTrivia)
+    }
+
+    // MARK: Helper methods
+
+    func setUpEventButtons(trivia: [SportsTrivia]) -> [UIButton] {
+        var triviaTitles = [String]()
+        for event in trivia {
+            triviaTitles.append(event.description)
+        }
+
+        eventButtons = [eventButtonOne, eventButtonTwo, eventButtonThree, eventButtonFour]
+        for button in eventButtons {
+            button.setTitle(triviaTitles.removeLast(), for: .normal)
+        }
+        return eventButtons
+    }
+
+
+
+
+    func createSportsTriviaOptions(trivia: [SportsTrivia]) -> [SportsTrivia] {
+        let randomTrivia = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: trivia) as! [SportsTrivia]
+        var roundOfTrivia: [SportsTrivia] = []
+        for event in randomTrivia {
+            if roundOfTrivia.count < eventsPerRound {
+                roundOfTrivia.append(event)
+            }
+        }
+        return roundOfTrivia
+    }
+
+    func sortedTrivia(trivia: [SportsTrivia]) -> [SportsTrivia] {
+        return trivia.sorted { $0.year < $1.year }
+    }
+
 
 }
 
